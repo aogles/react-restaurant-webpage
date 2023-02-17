e81bdeb9e80393f9ee5a39db27aeda7a374c5124
Amber Oglesby <aoglesby5795@gmail.com>
2023-02-17 14:50:14 -0500

    add local storage function

--- src/components/Order.js
+++ src/components/Order.js
@@ -7,16 +7,29 @@ const Order = ({ cart }) => {
   const [email, setEmail] = useState("");
   const [fullname, setFullname] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [items, setItems] = useState([]);
-  const [price, setPrice] = useState("");
 
   // save the complete order to localStorage
-  useEffect(() => {
-    localStorage.setItem(
-      "items",
-      JSON.stringify(items, email, fullname, phoneNumber, cart)
-    );
-  }, [items, email, fullname, phoneNumber, cart]);
+  //creates componenent that submits the order to local data, order is set to an object of all the required order information
+  //get items, will need to use parse to turn stringified array into normal array that can be used
+  //strigify will make strigified array in order to store in local storage
+  //.push is used to push order componnenets into the orders being stored
+
+  const submitOrder = () => {
+    const order = {
+      email,
+      fullname,
+      phoneNumber,
+      cart,
+    };
+
+    const orders = localStorage.getItem("orders")
+      ? JSON.parse(localStorage.getItem("orders"))
+      : [];
+
+    orders.push(order);
+
+    localStorage.setItem("orders", JSON.stringify(orders));
+  };
 
   const cartItems = cart.map((item, index) => (
     <div key={index}>
@@ -35,7 +48,7 @@ const Order = ({ cart }) => {
   // initialValue
   //current value is the first Index/price in the array, after removing $, use parseFlaot(bc of decimal), map through prices of cart and return the sum , initial value in cart is set to 0
   //reduce() method executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element
-
+  //onSubmit doe not go in the button, onSubmit wil call the function
   const subTotal = cart.reduce(
     (accumulator, i) => accumulator + parseFloat(i.price),
     0
@@ -45,13 +58,15 @@ const Order = ({ cart }) => {
     <>
       <div> Subtotal: ${subTotal} </div>
       {cartItems}
-      <Form>
+
+      <Form onSubmit={submitOrder}>
         <div className="container"></div>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <div></div>
+          {email}
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
+
           <Form.Label>Full Name</Form.Label>
           <Form.Control
             type="text"
@@ -62,7 +77,7 @@ const Order = ({ cart }) => {
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Phone Number</Form.Label>
           <Form.Control type="number" placeholder="Phone Number" />
-          <Button onClick={useEffect} variant="primary" type="submit">
+          <Button variant="primary" type="submit">
             Submit
           </Button>
         </Form.Group>
